b7e85285146535bb55c3a98aeca3d97d8a5b11aa
IndiraAkkiraju <indiraakkiraju21@gmail.com>
2022-12-04 15:48:53 -0500

    created and deployed first task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+        createTask("first task example");
+      }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
